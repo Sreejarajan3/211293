46919c7ae9e59e0e1019226ea512fd8d3daba481
sreeja <sreejarajan3@gmail.com>
2018-02-26 16:59:00 +0530

    Commit Files

--- WebApp1/src/srec/ac/com/WebApp.java
+++ WebApp1/src/srec/ac/com/WebApp.java
@@ -5,7 +5,7 @@ public class WebApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("Good Day!!");
-		System.out.println("great  Day!!"); 
+	
 	}
 
 }
